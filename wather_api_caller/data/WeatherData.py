fa3a3c7671fadf39aba8ff32afe0e83fc85d006f
yasuomaidana <yasuomaidana@gmail.com>
2023-07-25 16:19:53 +0900

    Updating WeatherData

--- wather_api_caller/data/WeatherData.py
+++ wather_api_caller/data/WeatherData.py
@@ -1,12 +1,17 @@
+from datetime import datetime
+
+
 class WeatherData:
-    def __init__(self, name: str, short_name: str, weather_status: str, temperature: float, humidity: float):
+    def __init__(self, name: str, short_name: str, weather_status: str, temperature: float, humidity: float,
+                 date: str):
         self.name = name
         self.short_name = short_name
         self.weather_status = weather_status
         self.temperature = temperature
         self.humidity = humidity
+        self.date = datetime.strptime(date, '%Y-%m-%d')
 
 
 if __name__ == "__main__":
-    weather = WeatherData("Beijing", "CN", "Partly cloudy", 36.5, 80)
+    weather = WeatherData("Beijing", "CN", "Partly cloudy", 36.5, 80, "2023-07-25")
     print(vars(weather))
