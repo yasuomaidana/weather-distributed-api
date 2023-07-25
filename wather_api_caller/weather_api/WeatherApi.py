ec027630600febd0bad1a8e39be336cfa266aaf3
yasuomaidana <yasuomaidana@gmail.com>
2023-07-25 20:24:11 +0900

    Implementing weather api

---
+++ wather_api_caller/weather_api/WeatherApi.py
@@ -0,0 +1,38 @@
+from typing import List
+
+import requests
+
+from wather_api_caller.configuration.Configuration import Configuration
+from wather_api_caller.countries.country_finder import find_country
+from wather_api_caller.data.WeatherData import WeatherData
+from datetime import datetime
+
+
+class WeatherApi:
+    def __init__(self, configuration_yaml: str):
+        config = Configuration(configuration_yaml).get_api()
+        self.endpoint = "https://" + config["host"] + config["request"]
+        self.header = {
+            'X-RapidAPI-Key': config["key"],
+            'X-RapidAPI-Host': config["host"]
+        }
+
+    def get_country_weather(self, place: str) -> List[WeatherData]:
+        country = find_country(place)
+        if country is None:
+            return []
+        query = {"q": country.city_name, "days": "3"}
+        res = requests.get(self.endpoint, headers=self.header, params=query)
+        data = res.json()
+        forecast = data["forecast"]
+        place_weather = []
+        for day in forecast["forecastday"]:
+            date = day["date"]
+            date = datetime.strptime(date, '%Y-%m-%d')
+            day = day["day"]
+            temp = day["avgtemp_c"]
+            status = day["condition"]["text"]
+            humidity = day["avghumidity"]
+            weather = WeatherData(country.city_name, country.country, country.short_name, status, temp, humidity, date)
+            place_weather.append(weather)
+        return place_weather
---
+++ wather_api_caller/weather_api/test_WeatherApi.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from wather_api_caller.weather_api.WeatherApi import WeatherApi
+
+
+class TestWeatherApi(TestCase):
+    def test_get_country_weather(self):
+        weather_api = WeatherApi("config_api_test")
+        real_place = weather_api.get_country_weather("London")
+        self.assertEqual(len(real_place), 3)
+        self.assertEqual(weather_api.get_country_weather("dx"), [])
