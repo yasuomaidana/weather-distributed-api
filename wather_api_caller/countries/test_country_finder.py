a481561d41993a32fddda7f7d840cec32001e567
yasuomaidana <yasuomaidana@gmail.com>
2023-07-25 20:23:51 +0900

    Updating request finder test

--- wather_api_caller/countries/test_country_finder.py
+++ wather_api_caller/countries/test_country_finder.py
@@ -1,13 +1,16 @@
 from unittest import TestCase
 
-from wather_api_caller.countries.country_finder import find_country
+from wather_api_caller.countries.country_finder import find_country, CountryName
 
 
 class Test(TestCase):
     def test_find_country(self):
-        self.assertEqual(find_country("is"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
-        self.assertEqual(find_country("IS"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
-        self.assertEqual(find_country("Republic of Iceland"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
-        self.assertEqual(find_country("Iceland"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
-        self.assertEqual(find_country("Reykjavik"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
-        self.assertEqual(find_country("reykjavik"), ['IS', 'Republic of Iceland', 'Iceland', 'Reykjavik'])
+        country = CountryName(short_name="IS", country="Republic of Iceland/Iceland", city_name="Reykjavik")
+        self.assertEqual(find_country("is"), country)
+        self.assertEqual(find_country("IS"), country)
+        self.assertEqual(find_country("Republic of Iceland"), country)
+        self.assertEqual(find_country("Iceland"), country)
+        self.assertEqual(find_country("Reykjavik"), country)
+        self.assertEqual(find_country("reykjavik"), country)
+
+        self.assertIsNone(find_country("dx"))
