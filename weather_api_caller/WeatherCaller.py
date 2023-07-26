bbaed537f59b02bb356e0a2b7f55afa516bf7e67
yasuomaidana <yasuomaidana@gmail.com>
2023-07-26 17:16:08 +0900

    Completing weather caller

--- weather_api_caller/WeatherCaller.py
+++ weather_api_caller/WeatherCaller.py
@@ -1,7 +1,13 @@
-from weather_api_caller.countries.country_finder import get_all_countries
+from weather_api_caller.countries.country_finder import get_all_countries, find_country
+from weather_api_caller.data.WeatherData import WeatherData
 from weather_api_caller.mongodb.MongoDataBase import MongoDataBase
 from weather_api_caller.weather_api.WeatherApi import WeatherApi
-from datetime import datetime, timedelta
+from datetime import datetime
+
+
+def get_today() -> datetime:
+    date = datetime.today().strftime('%Y-%m-%d')
+    return datetime.strptime(date, '%Y-%m-%d')
 
 
 class WeatherCaller:
@@ -9,23 +15,23 @@ class WeatherCaller:
         self.mongo_db = MongoDataBase(configuration)
         self.api = WeatherApi(configuration)
 
-    def get_weather(self):
-        date = datetime.today().strftime('%Y-%m-%d')
-
-        date = datetime.strptime(date, '%Y-%m-%d')
-        data = self.mongo_db.get_weather_by_date(date)
-        if len(data) == 0:
-            self.mongo_db.delete_old_weather(date)
-            for city, coordinate in get_all_countries():
-                if self.mongo_db.get_weather_by_date_and_place(date, city):
-                    continue
-                tomorrow = datetime.today().strftime('%Y-%m-%d')
-                tomorrow = datetime.strptime(tomorrow, '%Y-%m-%d') + timedelta(days=1)
-                if self.mongo_db.get_weather_by_date_and_place(tomorrow, city):
-                    continue
-                weathers = self.api.get_country_weather(city)
+    def update_all_weathers(self, date: datetime = get_today()):
+        for short_name, coordinate in get_all_countries():
+            if self.mongo_db.get_existing_weather(date, short_name):
+                continue
+            weathers = self.api.get_country_weather(short_name)
+            if weathers:
                 self.mongo_db.insert_weather(weathers)
 
-        if len(data) != 198:
-            pass
-        return data
+    def get_similar_weather(self, shortname: str, date=get_today()):
+        data = self.get_weather(shortname)
+        if data is None:
+            self.mongo_db.delete_old_weather(date)
+            self.update_all_weathers(date)
+            data = self.mongo_db.get_weather_by_date_and_place(date, find_country(shortname).short_name)
+        similar_weather = self.mongo_db.get_similar_weathers(date, data.weather_status)
+        similar_weather.insert(0, data)
+        return similar_weather
+
+    def get_weather(self, shortname: str, date=get_today()) -> WeatherData:
+        return self.mongo_db.get_weather_by_date_and_place(date, find_country(shortname).short_name)
--- weather_api_caller/test_WeatherCaller.py
+++ weather_api_caller/test_WeatherCaller.py
@@ -6,5 +6,13 @@ from weather_api_caller.WeatherCaller import WeatherCaller
 class TestWeatherCaller(TestCase):
     def test_get_weather(self):
         weather_caller = WeatherCaller("config_test")
-        weathers = weather_caller.get_weather()
-        self.assertGreater(len(weathers), 0)
+        weather_caller.update_all_weathers()
+        weathers = weather_caller.get_weather("GB")
+
+        self.assertIsNotNone(weathers)
+
+    def test_get_similar(self):
+        weather_caller = WeatherCaller("config_test")
+        weather_caller.update_all_weathers()
+        sim = weather_caller.get_similar_weather("mx")
+        self.assertGreater(len(sim), 0)
