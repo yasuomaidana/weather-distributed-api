067997a2e87a95a47b793dbc1190abd57cf27ad7
yasuomaidana <yasuomaidana@gmail.com>
2023-07-27 16:41:25 +0900

    Updating test

--- test_show_similar_weather.py
+++ test_show_similar_weather.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 
 from show_similar_weather import get_similar
+from weather_api_caller.countries.country_finder import get_all_countries
 
 
 class Test(TestCase):
     def test_get_similar(self):
-        get_similar("jp")
-
+        for short_name, i in get_all_countries():
+            ref, _ = get_similar(short_name)
+            self.assertEqual(short_name, ref[0].split(", ")[-1])
