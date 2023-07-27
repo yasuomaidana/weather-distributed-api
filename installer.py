5a61ceb1f973a6a989f534bac69aa51f776add0f
yasuomaidana <yasuomaidana@gmail.com>
2023-07-27 16:58:06 +0900

    Fixing installer

--- installer.py
+++ installer.py
@@ -7,10 +7,12 @@ def create_virtual_environment():
     # Create a virtual environment using venv
     subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
 
+
 def check_virtual_environment():
     if not os.path.exists("venv"):
         print(f"Creating virtual environment:")
         create_virtual_environment()
+
 
 def activate_virtual_environment():
     # Replace "venv" with the name of your virtual environment directory
@@ -18,10 +20,10 @@ def activate_virtual_environment():
 
     if os.name == "nt":
         activate_cmd = f"{venv_path}\\Scripts\\activate"
+        subprocess.run(activate_cmd, shell=True, check=True)
     else:
         activate_cmd = f"source {venv_path}/bin/activate"
-
-    subprocess.run(activate_cmd, shell=True, check=True)
+        subprocess.run(activate_cmd, shell=True, executable="/bin/bash", check=True)
 
 
 def install_required_libraries():
@@ -32,13 +34,14 @@ def install_required_libraries():
 
     for lib in required_libraries:
         try:
-            if lib == "pymongo[srv]":
-                lib = "pymongo"
-            if lib == "PyYAML":
-                lib = "yaml"
-            if lib == "scikit-learn":
-                lib = "sklearn"
-            __import__(lib)
+            lib_t = lib
+            if lib_t == "pymongo[srv]":
+                lib_t = "pymongo"
+            if lib_t == "PyYAML":
+                lib_t = "yaml"
+            if lib_t == "scikit-learn":
+                lib_t = "sklearn"
+            __import__(lib_t)
         except ImportError:
             print(f"Installing {lib}...")
             subprocess.run([sys.executable, "-m", "pip", "install", lib], check=True)
