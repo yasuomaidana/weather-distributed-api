0b716d1159a8b3b4ec29db14fb9d7ab6272904bc
yasuomaidana <yasuomaidana@gmail.com>
2023-08-03 17:53:17 +0900

    Including non listed countries

--- weather_api_caller/weather_api/WeatherApi.py
+++ weather_api_caller/weather_api/WeatherApi.py
@@ -5,7 +5,8 @@ from weather_api_caller.countries.country_finder import find_country
 from weather_api_caller.data.WeatherData import WeatherData
 from datetime import datetime
 
-
+def handle_call():
+    pass
 class WeatherApi:
     def __init__(self, configuration_yaml: str):
         config = Configuration(configuration_yaml).get_api()
@@ -17,19 +18,32 @@ class WeatherApi:
 
     def call_api(self, query):
         res = requests.get(self.endpoint, headers=self.header, params=query)
+        if not res.ok:
+            return None
         data = res.json()
         forecast = data["forecast"]
         place_weather = []
+        country = find_country(data["location"]["country"])
+        if country is None:
+            for i in data["location"]["country"].split():
+                country = find_country(i)
+                if country:
+                    break
+            if country is None:
+                country = data["location"]["country"]
+                letters = [word[0].upper() for word in country]
+                country = ''.join(letters)
+
         for day in forecast["forecastday"]:
-            date = day["date"]
-            date = datetime.strptime(date, '%Y-%m-%d')
-            day = day["day"]
-            temp = day["avgtemp_c"]
-            status = day["condition"]["text"]
-            humidity = day["avghumidity"]
-            weather = WeatherData(data["location"]["name"], data["location"]["country"], "From Query", status,
-                                  temp, humidity, date)
-            place_weather.append(weather)
+            for hour in day["hour"]:
+                date = hour["time"]
+                date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+                temp = hour["temp_c"]
+                status = hour["condition"]["text"]
+                humidity = hour["humidity"]
+                weather = WeatherData(data["location"]["name"], data["location"]["country"], country.short_name, status,
+                                      temp, humidity, date)
+                place_weather.append(weather)
         return place_weather
 
     def get_country_weather(self, place: str) -> list[WeatherData] | None:
--- weather_api_caller/weather_api/test_WeatherApi.py
+++ weather_api_caller/weather_api/test_WeatherApi.py
@@ -6,15 +6,8 @@ from weather_api_caller.weather_api.WeatherApi import WeatherApi
 class TestWeatherApi(TestCase):
     def test_get_country_weather(self):
         weather_api = WeatherApi("config_api_test")
-        real_place = weather_api.get_country_weather("London")
 
-        from_api = weather_api.call_api({"q": "London", "days": "3"})
-        ri = real_place[0]
-        fai = from_api[0]
+        from_api = weather_api.call_api({"q": "London", "days": "1"})
 
-        self.assertEqual(len(real_place), 3)
-        self.assertIsNone(weather_api.get_country_weather("dx"))
-
-        self.assertEqual(ri.temperature, fai.temperature)
-        self.assertEqual(ri.weather_status, fai.weather_status)
-        self.assertEqual(ri.humidity, fai.humidity)
+        self.assertEqual(len(from_api), 24)
+        self.assertIsNone(weather_api.call_api({"q": "3x", "days": "1"}))
